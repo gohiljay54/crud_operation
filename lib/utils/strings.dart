aa777d77c73d75c51f529d239daff1b6e31d3224
SIDDHARTHA COMPUTER <jaygohil5445@gmail.com>
2024-01-02 20:08:52 +0530

    -> add strings.dart

---
+++ lib/utils/strings.dart
@@ -0,0 +1,15 @@
+class Strings{
+
+  ///splash screen
+  static const String splashText = "Splash Text";
+
+  ///login screen
+  static const login = "Login";
+  static const submit = "Submit";
+  static const email = "Email";
+  static const emailEmpty = "Please enter email";
+  static const emailInvalid = "Please enter valid email";
+  static const userName = "User Name";
+  static const userNameEmpty = "Please enter user name";
+
+}
